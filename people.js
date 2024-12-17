5504728eabe88e12325094c68bdd45cae62cd910
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-12-17 09:50:24 +0100

    feat: delete json file and modules folder

--- people.js
+++ people.js
@@ -1,10 +1,12 @@
 const name = require('./names');
 const hobbies = require('./hobbies');
+const objHobbies = require('./hobbies');
 
 function newObj(){
+  const {hobbies} = objHobbies('Football', 'Basket', 'Dama')
   return {
     fullname: name('Paolo','Bitta'),
-    hobbies: hobbies('Football', 'Basket', 'Dama')
+    hobbies
     }
 }
 
